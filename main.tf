3989ea3b78212339e40e20927dfcf5aa2b103e7b
mikey1 <mikey1997@gmail.com>
2023-02-13 20:14:05 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -4,3 +4,19 @@ resource "aws_vpc" "myvpc" {
         Name = "mikey-vpc"
     }
 }
+
+resource "aws_subnet" "first-subnet" {
+    vpc_id = "aws_vpc.myvpc.id"
+    cidr_block = "192.168.1.0/24"
+    tags = {
+        Name = "subnet1"
+    }
+}
+
+resource "aws_subnet" "myvpc" {
+    vpc_id = "aws_vpc.myvpc.id"
+    cidr_block = "192.168.2.0/24"
+    tags = {
+        Name = "subnet2"
+    }
+}
