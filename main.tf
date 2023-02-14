79fce4550607499b3149b9155e131077be753132
mikey1 <mikey1997@gmail.com>
2023-02-14 15:14:17 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -22,8 +22,7 @@ resource "aws_route_table" "my-rt" {
     gateway_id = aws_internet_gateway.my-igw.id
   }
   route {
-    cidr_block = "192.168.1.0/24"
-    subnet_id = aws_subnet.firstsubnet.id
+    cidr_block = "192.168.0.0/16"
   }
 }
 
