6bc0eb7fa463f0973c3c6d9ba29e7e9584af1df0
mikey1 <mikey1997@gmail.com>
2023-02-14 12:09:33 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_instance" "prac" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.firstsubnet.id
     associate_public_ip_address = true
-    key_name = "geethu"
+    key_name = "mikey"
     tags = {
         Name = "mikey-1"
     }
