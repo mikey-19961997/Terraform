9a996429eba3139128c1173e9a6834faed0911c3
mikey1 <mikey1997@gmail.com>
2023-02-14 12:00:15 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -13,6 +13,20 @@ resource "aws_subnet" "firstsubnet" {
     }
 }
 
+resource "aws_route_table" "my-rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/24"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+}
+
+resource "aws_route_table_association" "my-art" {
+  subnet_id      = aws_subnet.firstsubnet.id
+  route_table_id = aws_route_table.my-rt.id
+}
+
 resource "aws_security_group" "my-sg" {
   vpc_id      = aws_vpc.myvpc.id
   tags = {
