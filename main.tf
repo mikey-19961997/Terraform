e318a9e97bc70f3ac3e8e272e3cc828699ae4e65
mikey1 <mikey1997@gmail.com>
2023-02-14 18:06:01 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "aws_security_group" "my-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
