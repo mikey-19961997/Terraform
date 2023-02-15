e546e7df2038b8dc911b6626f3998879b4ee5f13
mikey1 <mikey1997@gmail.com>
2023-02-15 17:20:42 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ resource "aws_subnet" "subnets" {
   count = 5
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.sub_cidr_block(count.index)
-  
+
   tags = {
-    Name = var.sub_name{count.index)}
+    Name = var.sub_name(count.index)
   }
 }
 
