56d5a2f5f6ef199c5c169f943cc2c8a60b1a348a
mikey1 <mikey1997@gmail.com>
2023-02-15 13:34:51 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block = "192.168.0.0/16"
-  tags = {
-    Name = "mikey-vpc"
-  }
-}
-
-resource "aws_subnet" "firstsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "192.168.1.0/24"
-  tags = {
-    Name = "subnet1"
-  }
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mikeybucket"
 }
