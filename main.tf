42bd6f141e74841f3bb0c7c943e980918b9aba83
mikey1 <mikey1997@gmail.com>
2023-02-15 14:07:13 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "firstsubnet" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = "192.168.1.0/24"
+  cidr_block = "10.0.1.0/24"
   tags = {
     Name = "subnet1"
   }
