5ff0f4c6e869b9bc3b3c49f3739a05c08327512b
mikey1 <mikey1997@gmail.com>
2023-02-15 22:33:08 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_details.cidr_block
-  Name = var.vpc_details.Name
+  tags = {
+    Name = var.vpc_details.Name
+  }
 }
 
 resource "aws_subnet" "subnets" {
   count = length(var.subnet_details.Name)
+  vpc_id = aws_vpc.myvpc.id
   cidr_block = cidrsubnet(var.vpc_details.cidr_block,8,count.index)
   tags = {
     Name = var.subnet_details.Name[count.index]
