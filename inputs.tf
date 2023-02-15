255d8338d8b1fb28657828031774c7e38850e7d5
mikey1 <mikey1997@gmail.com>
2023-02-15 15:42:17 +0530

    Added changes

--- inputs.tf
+++ inputs.tf
@@ -8,7 +8,22 @@ variable "cidr_block" {
     default = "10.0.0.0/16"
 }
 
-variable "sub_cidr_block" {
+variable "sub_cidr_block1" {
     type = string
     default = "10.0.1.0/24"
+}
+
+variable "sub_cidr_block2" {
+    type = string
+    default = "10.0.2.0/24"
+}
+
+variable "sub_cidr_block3" {
+    type = string
+    default = "10.0.3.0/24"
+}
+
+variable "sub_cidr_block4" {
+    type = string
+    default = "10.0.4.0/24"
 }
--- main.tf
+++ main.tf
@@ -7,8 +7,32 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "firstsubnet" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.sub_cidr_block
+  cidr_block = var.sub_cidr_block1
   tags = {
     Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "secondsubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.sub_cidr_block2
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_subnet" "thirdsubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.sub_cidr_block3
+  tags = {
+    Name = "subnet3"
+  }
+}
+
+resource "aws_subnet" "fourthsubnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.sub_cidr_block4
+  tags = {
+    Name = "subnet4"
   }
 }
