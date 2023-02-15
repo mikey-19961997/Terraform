464c2c819ba126e723748f8697b98d136f3a15d4
mikey1 <mikey1997@gmail.com>
2023-02-15 15:59:11 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "firstsubnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.sub_cidr_block1
   tags = {
-    Name = "subnet1"
+    Name = "s1"
   }
 }
 
@@ -17,7 +17,7 @@ resource "aws_subnet" "secondsubnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.sub_cidr_block2
   tags = {
-    Name = "subnet2"
+    Name = "s2"
   }
   depends_on = [
     aws_subnet.firstsubnet
@@ -28,7 +28,7 @@ resource "aws_subnet" "thirdsubnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.sub_cidr_block3
   tags = {
-    Name = "subnet3"
+    Name = "s3"
   }
   depends_on = [
     aws_subnet.secondsubnet
@@ -39,7 +39,7 @@ resource "aws_subnet" "fourthsubnet" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = var.sub_cidr_block4
   tags = {
-    Name = "subnet4"
+    Name = "s4"
   }
   depends_on = [
     aws_subnet.thirdsubnet
