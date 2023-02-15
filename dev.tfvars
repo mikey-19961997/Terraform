f47f1bae867441fc88f05e67b97750049faa303f
mikey1 <mikey1997@gmail.com>
2023-02-15 17:17:07 +0530

    Added changes

--- dev.tfvars
+++ dev.tfvars
@@ -1,6 +1,4 @@
 region = "us-east-2"
 cidr_block = "10.0.0.0/16"
-sub_cidr_block1 = "10.0.1.0/24"
-sub_cidr_block2 = "10.0.2.0/24"
-sub_cidr_block3 = "10.0.3.0/24"
-sub_cidr_block4 = "10.0.4.0/24"
+sub_cidr_block = ["10.0.1.0/24,10.0.2.0/24,10.0.3.0/24,10.0.4.0/24,10.0.5.0/24"]
+sub_name = ["s1,s2,s3,s4,s5"]
--- main.tf
+++ main.tf
@@ -5,43 +5,12 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-resource "aws_subnet" "firstsubnet" {
+resource "aws_subnet" "subnets" {
+  count = 5
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.sub_cidr_block1
+  cidr_block = var.sub_cidr_block(count.index)
   tags = {
-    Name = "s1"
+    Name = var.sub_name{count.index)}
   }
 }
 
-resource "aws_subnet" "secondsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.sub_cidr_block2
-  tags = {
-    Name = "s2"
-  }
-  depends_on = [
-    aws_subnet.firstsubnet
-    ]
-}
-
-resource "aws_subnet" "thirdsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.sub_cidr_block3
-  tags = {
-    Name = "s3"
-  }
-  depends_on = [
-    aws_subnet.secondsubnet
-  ]
-}
-
-resource "aws_subnet" "fourthsubnet" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.sub_cidr_block4
-  tags = {
-    Name = "s4"
-  }
-  depends_on = [
-    aws_subnet.thirdsubnet
-  ]
-}
